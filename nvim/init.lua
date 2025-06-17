0138f365b121b1355215d88a6753859af4872792
be-ep <eric@synergizetech.dev>
2025-06-17 09:53:18 +0000

    chores: open alpha dashboard on period or directory

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,17 @@
 require("config.lazy")
 require("config.options")
 
+-- only show alpha when no files or directories are opened
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    local is_dir = vim.fn.isdirectory(vim.fn.argv()[1] or "") == 1
+    if is_dir then
+      vim.cmd("cd " .. vim.fn.argv()[1])
+      -- Clear args so alpha shows
+      vim.cmd("silent! args")
+      require("alpha").start()
+    end
+  end
+})
+
 
