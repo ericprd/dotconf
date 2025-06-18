bc9798c659e5ee4875c241535223ba050fa90258
be-ep <eric@synergizetech.dev>
2025-06-18 02:48:57 +0000

    fix: autoformat gopls

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -8,19 +8,43 @@ return {
             local opts = { buffer = buffnr }
 
 	    	vim.keymap.set("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
---            vim.keymap.set("n", "gr", "<Cmd>lua vim.lsp.buf.references()<CR>", opts)
---
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                buffer = buffnr,
-                callback = function()
+
+            --vim.api.nvim_create_autocmd("BufWritePre", {
+              --  buffer = buffnr,
+                --callback = function()
                   -- Apply code action for organizing imports
-                  local context = { only = { "source.organizeImports" } }
+                  --local context = { only = { "source.organizeImports" } }
+                  --local params = vim.lsp.util.make_range_params()
+                  --params.context = context
+                  --vim.lsp.buf.code_action = {
+                      --context = context,
+                      --apply = true,
+                  --}
+                --end,
+            --})
+
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                buffer = bufnr,
+                callback = function()
+                  -- Format the buffer (blocking to ensure proper save state)
+                  vim.lsp.buf.format({ async = false })
+
+                  -- Organize imports
                   local params = vim.lsp.util.make_range_params()
-                  params.context = context
-                  vim.lsp.buf.code_action = {
-                      context = context,
-                      apply = true,
-                  }
+                  params.context = { only = { "source.organizeImports" } }
+
+                  local result = vim.lsp.buf_request_sync(bufnr, "textDocument/codeAction", params, 1000)
+                  if result then
+                    for _, res in pairs(result) do
+                      for _, action in pairs(res.result or {}) do
+                        if action.edit then
+                          vim.lsp.util.apply_workspace_edit(action.edit, "utf-8")
+                        elseif type(action.command) == "table" then
+                          vim.lsp.buf.execute_command(action.command)
+                        end
+                      end
+                    end
+                  end
                 end,
             })
         end
